eb826447e25443ceb2b0f7c66297cb372ed058ba
achrafash <aitsidihammou.achraf@gmail.com>
2024-04-17 19:42:02 +0100

    feat(audio-player): refactor audio player using @wavesurfer/react

--- src/app/audio-player.tsx
+++ src/app/audio-player.tsx
@@ -1,44 +1,41 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useState } from "react";
+import WavesurferPlayer from "@wavesurfer/react";
 import WaveSurfer from "wavesurfer.js";
 
 const SKIP_STEP = 2;
 
 export default function AudioPlayer(props: { url: string }) {
-    const waveformRef = useRef(null);
-    let wavesurfer: undefined | WaveSurfer;
-
-    // TODO: refactor without using `useEffect` (https://react.dev/learn/you-might-not-need-an-effect)
-    useEffect(() => {
-        if (!waveformRef.current) return;
-
-        wavesurfer = WaveSurfer.create({
-            container: waveformRef.current,
-            url: props.url,
-            waveColor: "#9ca3af",
-            progressColor: "#fb923c",
-            dragToSeek: true,
-            //   hideScrollbar: true,
-            normalize: true,
-            barGap: 1,
-            height: 60,
-            barHeight: 20,
-            autoCenter: true,
-            autoScroll: true,
-            cursorColor: "#c2410c",
-            // audioRate aka playback speed
-        });
-
-        return () => wavesurfer?.destroy();
-    });
+    const [wavesurfer, setWavesurfer] = useState<WaveSurfer>();
+    const [isPlaying, setIsPlaying] = useState(false);
 
     return (
         <div className="mx-auto flex max-w-xl flex-col items-center">
-            <div ref={waveformRef} className="w-full rounded border p-2" />
+            <div className="w-full">
+                <WavesurferPlayer
+                    url={props.url}
+                    waveColor={"#9ca3af"}
+                    progressColor={"#fb923c"}
+                    dragToSeek={true}
+                    normalize={true}
+                    barGap={1}
+                    height={60}
+                    barHeight={20}
+                    autoCenter={true}
+                    autoScroll={true}
+                    cursorColor={"#c2410c"}
+                    onReady={(wave) => {
+                        setWavesurfer(wave);
+                        setIsPlaying(false);
+                    }}
+                    onPlay={() => setIsPlaying(true)}
+                    onPause={() => setIsPlaying(false)}
+                />
+            </div>
             <div
                 id="controls"
-                className="flex w-full justify-center space-x-4 py-2 font-mono text-sm"
+                className="flex w-full justify-between space-x-4 py-2 font-mono text-sm"
             >
                 <div className="flex items-center space-x-4">
                     <button
@@ -51,9 +48,7 @@ export default function AudioPlayer(props: { url: string }) {
                     <button
                         onClick={() => wavesurfer && wavesurfer.playPause()}
                     >
-                        {wavesurfer && wavesurfer.isPlaying()
-                            ? "pause"
-                            : "play"}
+                        {isPlaying ? "pause" : "play"}
                     </button>
                     <button
                         onClick={() => wavesurfer && wavesurfer.skip(SKIP_STEP)}
